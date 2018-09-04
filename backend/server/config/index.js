9f59c6fd5c3216014ef4ef3ec88cf6e84568366e
Alexandr Gureev <g.alex00@bk.ru>
2018-09-05 02:17:57 +0300

    change .gitignore

--- backend/server/config/index.js
+++ backend/server/config/index.js
@@ -7,7 +7,7 @@ module.exports = {
   PORT: process.env.PORT || 3000,
   MONGO_URI: process.env.MONGO_URI || `mongodb://${user}:${password}@localhost:27017/${database}`,
   emailService: {
-    user: "",
-    pass: ""
+    user: "gureev.alex95@gmail.com",
+    pass: "469090812SsSs"
   }
 }
